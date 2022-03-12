12eb6d7b0c557baf18bb8919be5c35858de42c4c
epneeh <m.efni.s@gmail.com>
2022-03-12 14:32:20 +0700

    update

--- router.py
+++ router.py
@@ -1,3 +1,4 @@
+from ctypes import util
 from jinja2 import Environment, PackageLoader, select_autoescape
 from sanic import json
 from sanic.response import html
@@ -14,8 +15,20 @@ BASE_URL = "http://192.168.0.110:9090/"
 
 async def index(request):
     template = env.get_template('index.html')
+    cpu_util = utils.cpu_percent()
+    cpu_freq = utils.cpu_freq()
+    cpu_temp = utils.cpu_temp()
+    ram = utils.ram()
+    swap = utils.swap()
+    
     data = {
-        "base_url" : BASE_URL
+        "base_url" : BASE_URL,
+        "judul" : "Raspi System Info",
+        "cpu_percent" : cpu_util,
+        "cpu_freq" : cpu_freq,
+        "temps" : cpu_temp,
+        "ram" : ram,
+        "swap" : swap
         }
     return html(template.render(data))
 
@@ -28,5 +41,16 @@ async def static(request,pth):
 
 async def info(request):
     type = request.form.get("type")
-
-    return json()
+    if type == "cpu_util":
+        j = utils.cpu_percent()
+    elif type == "freq":
+        j =  utils.cpu_freq()
+    elif type == "temp":
+        j = utils.cpu_temp()
+    elif type == "ram":
+        j = utils.ram()
+    elif type == "swap":
+        j = utils.swap()
+    else:
+        j = {}
+    return json(j)
--- utils.py
+++ utils.py
@@ -1,2 +1,24 @@
 import psutil
 
+def cpu_percent():
+    return psutil.cpu_percent(1,True)
+
+def cpu_freq():
+    return psutil.cpu_freq(True)
+
+def cpu_count():
+    return psutil.cpu_count(True)
+
+def cpu_temp():
+    cpu_temps = psutil.sensors_temperatures();
+    temps=[]
+    for name,entries in cpu_temps.items():
+        for entry in entries:
+            temps.append({"name" : entry.label or name,"current" : entry.current})
+    return temps
+
+def ram():
+    return psutil.virtual_memory()
+
+def swap():
+    return psutil.swap_memory()
