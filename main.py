cd30b85732df30b453d26a5fdfc84af5b03c8326
epneeh <m.efni.s@gmail.com>
2022-03-12 09:00:08 +0700

    update

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app=Sanic(__name__)
 
 app.add_route(R.index,"/",methods=['GET'])
 app.add_route(R.static,"/static/<pth:path>",methods=['GET'])
-app.add_route(R.info,"/info/<type:str>",methods=['POST'])
+app.add_route(R.info,"/info",methods=['POST'])
 
 if __name__ == '__main__':
     app.run("0.0.0.0",9090,fast=True)
--- router.py
+++ router.py
@@ -1,19 +1,23 @@
 from jinja2 import Environment, PackageLoader, select_autoescape
-from numpy import empty
 from sanic import json
 from sanic.response import html
 from sanic.response import file
 from os.path import exists
-
+import utils
 
 env = Environment(
     loader=PackageLoader(__name__),
     autoescape=select_autoescape(['html'])
 )
 
+BASE_URL = "http://192.168.0.110:9090/"
+
 async def index(request):
     template = env.get_template('index.html')
-    return html(template.render())
+    data = {
+        "base_url" : BASE_URL
+        }
+    return html(template.render(data))
 
 async def static(request,pth):
     sfile = './static/' + pth
@@ -22,6 +26,7 @@ async def static(request,pth):
     else:
         return html('',404)
 
-async def info(request,type):
+async def info(request):
+    type = request.form.get("type")
 
     return json()
